59d2780510d7e68d6fc382df92a2e76cc49f8620
Kendall Mackey <kendallmackey25@gmail.com>
2024-02-05 14:43:48 -0800

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,7 +1,6 @@
 variable "default-tags" {
   type = map(string)
   default = {
-    "owner" = "kmackey"
     "team" = "red"
   }
   description = "This is a resource in my terraform testing environment"
